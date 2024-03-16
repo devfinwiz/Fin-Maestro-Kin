e5b3e138d6ae828be5259eb7382ea666607dafae
Suyash_Musale <suyashdmusale@gmail.com>
2024-03-16 18:00:29 +0530

    Added modules for testing endpoints

---
+++ constants/endpoints.py
@@ -0,0 +1,21 @@
+ENDPOINTS = [
+    '/equities/security-archives?symbol=TCS&start_date=04-01-2024&end_date=14-01-2024&series=ALL',
+    '/equities/bulk-deals-archives?start_date=28-01-2024&end_date=04-02-2024',
+    '/equities/block-deals-archives?start_date=28-01-2024&end_date=04-02-2024',
+    '/equities/short-selling-archives?start_date=28-01-2024&end_date=04-02-2024',
+    '/equities/corporate-actions?start_date=28-01-2024&end_date=04-02-2024',
+    '/equities/most-active-securities',
+    '/equities/advances-declines?year=2024',
+    '/equities/monthly-settlement-stats/capital-market?financial_year=2022-2023',
+    '/equities/monthly-settlement-stats/fno?financial_year=2022-2023',
+    '/equities/stock-pcr?symbol=RELIANCE',
+    '/equities/equity-tickers',
+    '/nseindices/history?symbol=NIFTY 50&start_date=10-Jan-2024&end_date=12-Jan-2024',
+    '/nseindices/ratios?symbol=NIFTY 50&start_date=10-Jan-2024&end_date=12-Jan-2024',
+    '/nseindices/returns?symbol=NIFTY 50&start_date=10-Jan-2024&end_date=12-Jan-2024',
+    '/nseindices/indice-pcr?symbol=NIFTY',
+    '/nseindices/india-vix?start_date=28-01-2024&end_date=04-02-2024',
+    '/sentiment/pcr-indice-analysis',
+    '/sentiment/pcr-stocks-analysis?symbol=INFY',
+    '/generate_plot?ticker=BSE.NS'
+]
