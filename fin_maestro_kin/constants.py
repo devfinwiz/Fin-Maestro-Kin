58421f7b1a977eb7d643c0f812ccd63a14aab774
DEV_FINWIZ <devjuneja43@gmail.com>
2024-04-20 11:02:37 +0530

    introduce new endpoints for data from screener.in

--- fin_maestro_kin/constants.py
+++ fin_maestro_kin/constants.py
@@ -23,5 +23,10 @@ ENDPOINTS = [
     '/sentiment/pcr-indice-analysis',
     '/sentiment/pcr-stocks-analysis?symbol=INFY',
     '/generate_plot?ticker=BSE.NS',
-    '/screener-equities/quarterly-result?symbol=vedl'
+    '/screener-equities/quarterly-result?symbol=vedl',
+    '/screener-equities/shareholding-pattern?symbol=vedl',
+    '/screener-equities/cash-flow?symbol=vedl',
+    '/screener-equities/balance-sheet?symbol=vedl',
+    '/screener-equities/annual-profit-loss?symbol=vedl',
+    '/screener-equities/ratios?symbol=vedl'
 ]
