f13b83b4de6f1df9dc24f435645ebd8e8c6d6e4a
devfinwiz <devjuneja43@gmail.com>
2025-05-27 00:52:51 +0530

    meet the prerequisites for valuations computation

--- fin_maestro_kin/modules/data_toolkit/screener/screener_equities.py
+++ fin_maestro_kin/modules/data_toolkit/screener/screener_equities.py
@@ -79,7 +79,7 @@ class ScreenerEquities(Helper):
         app.include_router(self.router)
 
     def quarterly_results(self, company_code):
-        url = f"https://www.screener.in/company/{company_code}/#quarters"
+        url = f"https://www.screener.in/company/{company_code}/consolidated/#quarters"
         response = requests.get(url)
         return self.handle_response(response, company_code, 'quarters')
 
@@ -167,10 +167,85 @@ class ScreenerEquities(Helper):
     def get_key_metrics(self, symbol: str = Query(..., title="Symbol", description="Stock Symbol")):
         try:
             response = self.key_metrics(symbol)
-            if response is not None:
-                processed_data = response
-                return JSONResponse(content=processed_data)
-            else:
-                return JSONResponse(content={"error": "Failed to fetch data from URL"}, status_code=500)
+            if response is None:
+                return JSONResponse(content={"error": "Failed to fetch key metrics data"}, status_code=500)
+            processed_data = response
+
+            #-----------------------------------------------------
+            # Add last 4 quarter's eps and ttm eps in key metrics
+            # Add last 4 quarter's OPM % in key metrics
+            #-----------------------------------------------------
+            quarterly_data = self.quarterly_results(symbol)
+            quarters_data = self.process_data(quarterly_data, 'quarters')
+            hold = quarters_data.get("quarters", {})
+            eps_dict = hold.get("EPS in Rs", {})
+
+            if eps_dict:
+                eps_values = [eps_dict[q] for q in list(eps_dict.keys())[-4:] if eps_dict[q] not in ["", "-", None]]
+                eps_values_float = []
+
+                for v in eps_values:
+                    try:
+                        eps_values_float.append(float(v))
+                    except Exception:
+                        pass 
+
+                for i, eps_val in enumerate(eps_values_float, start=1):
+                    processed_data["key_metrics"][f"EPS (Q{i})"] = str(eps_val)
+
+                ttm_eps = round(sum(eps_values_float), 2) if eps_values_float else None
+                if ttm_eps is not None:
+                    processed_data["key_metrics"]["EPS (TTM)"] = str(ttm_eps)
+
+            opm_percentage = hold.get("OPM %", {})
+            if opm_percentage:
+                opm_percentage = [opm_percentage[q] for q in list(opm_percentage.keys())[-4:] if opm_percentage[q] not in ["", "-", None]]
+                
+                for i, opm_val in enumerate(opm_percentage, start=1):
+                    processed_data["key_metrics"][f"OPM% (Q{i})"] = opm_val
+
+            #-----------------------------------------------------
+            # Add last 4 quarter's promoter holding in key metrics
+            # Add change in promoter holding in key metrics
+            #-----------------------------------------------------
+            shareholding_pattern = self.shareholding_pattern(symbol)
+            shareholding_data = self.process_data(shareholding_pattern, 'shareholding')
+            hold = shareholding_data.get("shareholding", {})
+            promoter_holding = hold.get("Promoters", {})
+
+            if promoter_holding:
+                promoter_holding = [promoter_holding[q] for q in list(promoter_holding.keys())[-4:] if promoter_holding[q] not in ["", "-", None]]
+                change_in_promoter_holding = round(float(promoter_holding[0]) - float(promoter_holding[3]), 2)
+
+                for i, promoter_value in enumerate(promoter_holding, start=1):
+                    processed_data["key_metrics"][f"Promoters (Q{i})"] = promoter_value
+                processed_data["key_metrics"][f"Change in promoter holding"] = str(change_in_promoter_holding)
+            
+            #-----------------------------------------------------
+            # Add P/S ratio in key metrics
+            #-----------------------------------------------------
+            hold = quarters_data.get("quarters", {})
+            sales_data = hold.get("Sales", {})
+
+            if sales_data:
+                sales_data = [sales_data[q] for q in list(sales_data.keys())[-4:] if sales_data[q] not in ["", "-", None]]
+                sales_values_float = []
+
+                for v in sales_data:
+                    try:
+                        sales_values_float.append(float(v))
+                    except Exception:
+                        pass 
+            
+                ttm_sales = round(sum(sales_values_float), 2) if sales_values_float else None
+                if ttm_sales is not None:
+                    processed_data["key_metrics"]["Sales (TTM)"] = str(ttm_sales)
+            
+                mcap = processed_data["key_metrics"]["Market Cap"]
+                price_to_sales = round(float(mcap) / ttm_sales, 2)
+                processed_data["key_metrics"]["Price/Sales"] = str(price_to_sales)
+
+            return JSONResponse(content=processed_data)
+
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Error fetching key metrics data: {e}")
